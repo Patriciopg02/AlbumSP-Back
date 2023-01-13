93fe8b0e3704e70afb8e52e58ead8d5021ec8952
Patriciopg02 <patriciopg02_@hotmail.com>
2023-01-13 14:15:23 -0300

    editar memorias

--- controllers/memory.controller.js
+++ controllers/memory.controller.js
@@ -1,24 +1,54 @@
 import Memory from '../models/Memory.js';
 
-export const getMemory = async(req,res) => {
+export const getMemory = async (req, res) => {
     try {
         const Memorys = await Memory.find();
         return res.send(Memorys);
     }
-    catch(err) {
+    catch (err) {
         return res.status(400).send(err);
     }
 }
 
-export const postMemory = async(req,res) => {
+export const postMemory = async (req, res) => {
     try {
-        const {image,date,description} = req.body;
-        const memory = new Memory({image,date,description});
+        const { image, date, description } = req.body;
+        const memory = new Memory({ image, date, description });
         await memory.save();
         return res.status(200).json(memory);
     }
-    catch(err) {
+    catch (err) {
         console.log(err);
     }
-    
 }
+
+export const putMemory = async (req, res) => {
+    try {
+        const { idMemory, date, description } = req.body;
+
+        const currentMemory = await memorySchema.findOne({ _id: idMemory });
+
+        currentMemory.date = date;
+        currentMemory.description = description;
+
+        const memoryUpdated = await memorySchema.findByIdAndUpdate(
+            {
+                _id: idMemory,
+            },
+            currentMemory,
+            {
+                new: true,
+            }
+        );
+
+        return res.status(200).json({
+            data: memoryUpdated,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            msg: `An error ocurred 😡`,
+            error,
+        });
+    }
+};
--- models/Memory.js
+++ models/Memory.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
 const memorySchema = new Schema({
-    image: {type: String, required: true},
+    image: {type: String, required: false},
     date: {type: String, required: true},
     description: {type: String, required: true},
 })
