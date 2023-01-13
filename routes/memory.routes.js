fbac7fb41e51627bb89c1bf86b93a8cbd21b4690
Patriciopg02 <patriciopg02_@hotmail.com>
2023-01-13 19:13:35 -0300

    put

--- routes/memory.routes.js
+++ routes/memory.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { getMemory, postMemory } from '../controllers/memory.controller.js'
+import { getMemory, postMemory, putMemory } from '../controllers/memory.controller.js'
 const router = Router();
 
-router.route('/').get(getMemory).post(postMemory);
+router.route('/').get(getMemory).post(postMemory).put(putMemory);
 
 export default router;
